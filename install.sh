3f6c262160f7ecb66d325ac0fa8728d5e9fb0f6b
Louis Kedziora <louie_101@live.ca>
2020-01-18 17:29:10 -0800

    added install.sh file

---
+++ install.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+# https://developers.supportbee.com/blog/setting-up-cucumber-to-run-with-Chrome-on-Linux/
+# https://gist.github.com/curtismcmullan/7be1a8c1c841a9d8db2c
+# http://stackoverflow.com/questions/10792403/how-do-i-get-chrome-working-with-selenium-using-php-webdriver
+# http://stackoverflow.com/questions/26133486/how-to-specify-binary-path-for-remote-chromedriver-in-codeception
+# http://stackoverflow.com/questions/40262682/how-to-run-selenium-3-x-with-chrome-driver-through-terminal
+# http://askubuntu.com/questions/760085/how-do-you-install-google-chrome-on-ubuntu-16-04
+
+# Versions
+CHROME_DRIVER_VERSION=`curl -sS chromedriver.storage.googleapis.com/LATEST_RELEASE`
+
+# Install dependencies.
+sudo apt-get update
+sudo apt-get install -y unzip openjdk-8-jre-headless xvfb libxi6 libgconf-2-4
+
+# Install ChromeDriver.
+sudo apt-get install chromium-browser
+
+wget -N http://chromedriver.storage.googleapis.com/$CHROME_DRIVER_VERSION/chromedriver_linux64.zip -P ~/
+unzip ~/chromedriver_linux64.zip -d ~/
+rm ~/chromedriver_linux64.zip
+sudo mv -f ~/chromedriver /usr/local/bin/chromedriver
+sudo chown root:root /usr/local/bin/chromedriver
+sudo chmod 0755 /usr/local/bin/chromedriver
+
+sudo apt install python3-pip
+pip3 install selenium
+pip3 install bs4
